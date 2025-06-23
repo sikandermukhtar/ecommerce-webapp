766b360cd8bad15e4ac1c094c7c810b6badc5f64
Sikandar-chaudhry <sikandarmukhtar5@gmail.com>
2025-06-23 20:29:14 +0500

    deleted:    frontend/.gitignore 	deleted:    frontend/README.md 	deleted:    frontend/eslint.config.mjs 	deleted:    frontend/next.config.ts 	deleted:    frontend/package-lock.json 	deleted:    frontend/package.json 	deleted:    frontend/postcss.config.mjs 	deleted:    frontend/public/file.svg 	deleted:    frontend/public/globe.svg 	deleted:    frontend/public/next.svg 	deleted:    frontend/public/vercel.svg 	deleted:    frontend/public/window.svg 	deleted:    frontend/src/app/admin/page.tsx 	deleted:    frontend/src/app/favicon.ico 	deleted:    frontend/src/app/globals.css 	deleted:    frontend/src/app/layout.tsx 	deleted:    frontend/src/app/page.tsx 	deleted:    frontend/src/components/admin/CategoryForm.tsx 	deleted:    frontend/src/components/admin/CategoryList.tsx 	deleted:    frontend/src/components/admin/SubCategoryForm.tsx 	deleted:    frontend/src/components/admin/SubSubCategoryForm.tsx 	deleted:    frontend/src/lib/api.ts 	deleted:    frontend/tsconfig.json

--- frontend/src/lib/api.ts
+++
@@ -1,9 +0,0 @@
-// src/lib/api.ts
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'http://localhost:8000',// ✅ Your FastAPI server
-  withCredentials: true,            // ✅ If you're using cookies
-});
-
-export default api;
